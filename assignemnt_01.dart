5e4fbdcfe6c34d94493b3c342748d9f90903e557
Md. Nazim Uddin <37615644+nazimfeni@users.noreply.github.com>
2023-08-02 21:46:56 +0600

    Add files via upload

---
+++ assignemnt_01.dart
@@ -0,0 +1,16 @@
+//Assignement-01 of module 01
+void main() {
+  const a = 7;
+  const b = 3;
+  var add = a + b;
+  var sub = a - b;
+  var multi = a * b;
+  var div = a / b;
+  var mod = a % b;
+
+  print("Addition: ${a} + ${b} = ${add}");
+  print("Subtraction: ${a} - ${b} = ${sub}");
+  print("Multiplication: ${a} * ${b} = ${multi}");
+  print("Division: ${a} / ${b} = ${div}");
+  print("Division: ${a} % ${b} = ${mod}");
+}
